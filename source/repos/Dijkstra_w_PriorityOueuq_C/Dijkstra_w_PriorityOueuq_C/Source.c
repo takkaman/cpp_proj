1fc6e4611f2aa25acb310aa01ccc4343000634d7
Leo Yan <phyan@internal.synopsys.com>
2018-05-15 19:12:30 +0800

    dijkstra init

---
+++ source/repos/Dijkstra_w_PriorityOueuq_C/Dijkstra_w_PriorityOueuq_C/Source.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  
+#include <string.h>  
+#include <stdlib.h>  
+#include "Graph.h"
+#include "Dijkstra.h"
+#include "PQ.h"
+
+int main() {
+	printf("Hello");
+
+	system("pause");
+	return 0;
+}
