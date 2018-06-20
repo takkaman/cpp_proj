ca3a029a2e31ea416429eb3b4fa6f169e1cb4ad9
Leo Yan <phyan@internal.synopsys.com>
2018-06-20 22:01:15 +0800

    update

---
+++ source/repos/Dijkstra_w_PriorityQueuq_C/Ass2_Testing/Ass-2/Graph.c
@@ -0,0 +1,47 @@
+// Graph ADT interface for Ass2 (COMP2521)
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "Graph.h"
+
+struct GraphRep{
+
+};
+
+
+
+Graph newGraph(int noNodes) {
+	return NULL;
+}
+
+int numVerticies(Graph g) {
+	return 0;
+}
+
+void  insertEdge(Graph g, Vertex src, Vertex dest, int weight) {
+	
+}
+
+void  removeEdge(Graph g, Vertex src, Vertex dest) {
+
+}
+
+bool adjacent(Graph g, Vertex src, Vertex dest) {
+	return false;
+}
+
+AdjList outIncident(Graph g, Vertex v) {
+	return NULL;
+}
+
+AdjList inIncident(Graph g, Vertex v) {
+	return NULL;
+}
+
+void  showGraph(Graph g) {
+
+}
+
+void  freeGraph(Graph g) {
+
+}
